7640e5e8f97d4d3652f65efbe2d8a032c7d70a3a
Ethan Friedman <ethanjfriedman@gmail.com>
2016-10-10 21:07:40 -0500

    got create a new user working

--- users.js
+++ users.js
@@ -8,53 +8,56 @@ import bcrypt from 'bcrypt';
 
 const usersController = Router();
 
+// for testing
 usersController.get('/', (req, res) => {
   res.json({success: true, error: null, message: 'To log in, go to /users/login'})
 });
-//
-// usersController.post('/new', (req, res) => {
-//   User.findOne({
-//     username: req.body.username
-//   }, (mongooseError, user) => {
-//     if (mongooseError) {
-//       res.json({success: false, token: null, error: 'Error searching db.'});
-//     } else if (user) {
-//       res.json({success: false, token: null, error: 'Pre-existing user with that username.'});
-//     } else {
-//       bcrypt.genSalt(10, (bcryptErr, salt) => {
-//         if (bcryptErr) {
-//           console.error.bind(console, `Error in bcrypt: ${bcryptErr}`);
-//           res.json({success: false, token: null, error: 'server error'});
-//         } else {
-//           bcrypt.hash(req.body.password, salt, (hashErr, hash) => {
-//             if (hashErr) {
-//               console.error.bind(console, `Error hashing password: ${hashErr}`);
-//               res.json({success: false, token: null, error: 'server error'});
-//             } else {
-//               User.save({
-//                 username: req.body.username,
-//                 password: hash,
-//                 teacher: req.body.isTeacher,
-//                 admin: req.body.isAdmin //TODO obvs needs work
-//               }, (saveErr, user) => {
-//                 if (saveErr) {
-//                   console.error.bind(console, `error saving new user: ${saveErr}`);
-//                   res.json({success: false, token: null, error: 'server error'});
-//                 } else {
-//                   // generate token for newly created user
-//                   const token = jwt.sign(user, config.secret, {
-//                     expiresIn: 1440 // 24 hours (in minutes)
-//                   });
-//                   res.json({success: true, error: null, token});
-//                 }
-//               });
-//             }
-//           });
-//         }
-//       });
-//     }
-//   })
-// });
+
+// create a new user
+usersController.post('/new', (req, res) => {
+  User.findOne({
+    username: req.body.username
+  }, (mongooseError, user) => {
+    if (mongooseError) {
+      res.json({success: false, token: null, error: 'Error searching db.'});
+    } else if (user) {
+      res.json({success: false, token: null, error: 'Pre-existing user with that username.'});
+    } else {
+      bcrypt.genSalt(10, (bcryptErr, salt) => {
+        if (bcryptErr) {
+          console.error.bind(console, `Error in bcrypt: ${bcryptErr}`);
+          res.json({success: false, token: null, error: 'server error'});
+        } else {
+          bcrypt.hash(req.body.password, salt, (hashErr, hash) => {
+            if (hashErr) {
+              console.error.bind(console, `Error hashing password: ${hashErr}`);
+              res.json({success: false, token: null, error: 'server error'});
+            } else {
+              const newUser = new User({
+                username: req.body.username,
+                password: hash,
+                teacher: req.body.isTeacher,
+                admin: req.body.isAdmin //TODO obvs needs work
+              });
+              newUser.save( saveErr => {
+                if (saveErr) {
+                  console.error.bind(console, `error saving new user: ${saveErr}`);
+                  res.json({success: false, token: null, error: 'server error'});
+                } else {
+                  // generate token for newly created user
+                  const token = jwt.sign(newUser, config.secret, {
+                    expiresIn: 1440 // 24 hours (in minutes)
+                  });
+                  res.json({success: true, error: null, token});
+                }
+              }); // end User.save
+            }
+          }); // end bcrypt.hash
+        }
+      }); // end bcrypt.genSalt
+    }
+  }); // end User.findOne
+}); //end route
 
 usersController.post('/login', (req, res) => {
   User.findOne({
